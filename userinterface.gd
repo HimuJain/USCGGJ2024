8ef1100d0278feae8b841b415e62bd80472ccda7
Himuchacha <jainhimanshu2006@gmail.com>
2024-01-27 20:34:25 -0800

    Progress bar color change code

---
+++ userinterface.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_color_changing_bar()
+	pass
+
+
+
+func _color_changing_bar():
+	if($volumeBar.value < 500):
+		$volumeBar.set_tint_progress(Color((($volumeBar.value) / 500),1, 0, 1))
+	if($volumeBar.value > 500):
+		$volumeBar.set_tint_progress(Color(1, (2-(($volumeBar.value) / 500)), 0, 1))
