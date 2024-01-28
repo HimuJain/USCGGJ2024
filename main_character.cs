85e9b6124ece4ef0915270d203d2f07f753d040b
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-01-27 18:11:47 -0800

    heehee

--- main_character.cs
+++ main_character.cs
@@ -4,13 +4,14 @@ using System;
 public partial class main_character : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -900.0f;
+	public const float JumpVelocity = -500.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	//public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public float gravity = 1800.0f;
 	public override void _PhysicsProcess(double delta)
 	{
+
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
